58adf066ba400d433c657d270eb148fcc1eb6d41
ironbrock <brock@ironheights.com>
2014-09-04 04:46:04 -0700

    Created Post  controller function and view page

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -9,4 +9,22 @@ class PostsController extends AppController {
         $this->set('posts',$this->Post->find('all'));
     }
 
+    /**
+     * @param null $id
+     * @throws NotFoundException
+     *
+     * Checks for non null argument, then checks for requested item not found in
+     * db.  Finally, returns the result.
+     */
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+    }
 }
